014c8528d9455a2b5379e1e27ffa1dfd5e0204ee
Akshita Mishra <167016134+Akshita7844@users.noreply.github.com>
2025-03-23 20:12:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -44,13 +44,15 @@ try:
         st.sidebar.success(f"✅ Loaded: {os.path.basename(selected_model_path)}")
     else:
         model = joblib.load('models/partner_model.pkl')
-         #Patch Fix: Add dummy monotonic_cst attribute if missing (for compatibility)
-        if not hasattr(model, 'monotonic_cst'):
-            model.monotonic_cst = None  # Or use [] if your code expects a list
         st.sidebar.info("Using sample model.")
+
+    # 🛠️ Patch: Add dummy monotonic_cst attribute if missing
+    if not hasattr(model, 'monotonic_cst'):
+        model.monotonic_cst = None  # Or use []
 except Exception as e:
     st.sidebar.error(f"❌ Model load error: {e}")
     st.stop()
+
 
 
 # --- Load Data ---
